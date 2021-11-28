ff075e9333c71805a3905b4abef8d03369190b9e
Henrik Olsson <hnrklssn@gmail.com>
2021-11-28 15:42:51 +0000

    Implemented a form for sending colors to blajt.

--- python-client/main.py
+++ python-client/main.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
 from bottle import run, get, post, request, route, template
-#from controller import set_color, set_all, cycle_colors
+from controller import set_color, set_all, cycle_colors
 
 @route('/')
 def index():
-    return template('<b> Blajt says hello</b>!')
+   return '''
+      <form action="/set_all_colors" method="post">
+        Red: <input name="red" type="number" /></br>
+        Green: <input name="green" type="number" /></br>
+        Blue: <input name="blue" type="number" /></br>
+        <input value="Set color" type="submit" />
+      </form>
+   '''
 
 @post('/cycle_colors')
 def cycle():
@@ -14,21 +21,21 @@ def cycle():
 
 @post('/set_color')
 def set_light_colors():
-    pixel = request.forms.get('pixel')
-    red = request.forms.get('red')
-    green = request.forms.get('green')
-    blue = request.forms.get('blue')
+    pixel = int(request.forms.get('pixel'))
+    red = int(request.forms.get('red'))
+    green = int(request.forms.get('green'))
+    blue = int(request.forms.get('blue'))
 
-    #set_color(pixel, red, green, blue)
+    set_color(pixel, red, green, blue)
     return template("Setting colors ({{red}}, {{green}}, {{blue}}) on pixel {{pixel}}", red=red, green=green, blue=blue, pixel=pixel)
 
 @post('/set_all_colors')
 def set_all_colors():
-    red = request.forms.get('red')
-    green = request.forms.get('green')
-    blue = request.forms.get('blue')
+    red = int(request.forms.get('red'))
+    green = int(request.forms.get('green'))
+    blue = int(request.forms.get('blue'))
 
-    #set_all(red, green, blue)
+    set_all(red, green, blue)
     return template("Setting all pixels to colors ({{red}}, {{green}}, {{blue}})", red=red, green=green, blue=blue)
 
-run(host='localhost', port=8080)
+run(host='192.168.7.241', port=8080)
