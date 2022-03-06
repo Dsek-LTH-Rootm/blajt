c2b34995022526a0930e79eb6bbd0006d48b7199
Henrik Olsson <hnrklssn@gmail.com>
2022-03-06 11:48:46 +0000

    Restructured and cleaned repository

---
+++ debug/blinkatest.py
@@ -0,0 +1,16 @@
+# script to test that the necessary hw resources are available
+import board
+import digitalio
+import busio
+
+print("Hello blinka!")
+
+# Try to great a Digital input
+pin = digitalio.DigitalInOut(board.D4)
+print("Digital IO ok!")
+
+# Try to create an SPI device
+spi = busio.SPI(board.SCLK, board.MOSI, board.MISO)
+print("SPI ok!")
+
+print("done!")
